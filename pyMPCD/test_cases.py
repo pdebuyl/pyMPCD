3b5187eef852b67bed5d6f15afb495858c3b0f4d
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2011-08-22 00:25:19 -0400

    Modified temperature gradient test case.

--- pyMPCD/test_cases.py
+++ pyMPCD/test_cases.py
@@ -78,16 +78,16 @@ def eight_PBC_wallx_tempgrad_8():
     Periodic boundary conditions in y and z.
     Thermostatted walls in direction x with temperature of .13 and .53.
     """
-    a = MPCD_system( (8,8,8) , 10, 1.)
+    a = MPCD_system( (32,8,8) , 10, 1.)
     a.null_shift()
     print a
     a.tau = .5
     a.wall_v0[0,0,:] = np.array( [ 0., .0, 0. ] )
     a.wall_v0[0,1,:] = np.array( [ 0., -.0, 0. ] )
-    a.wall_temp[0,0] = 0.33*1.2
-    a.wall_temp[0,1] = 0.33*0.8
+    a.wall_temp[0,0] = 0.37
+    a.wall_temp[0,1] = 0.33
     a.init_r()
-    a.init_v(.33)
+    a.init_v(.35)
     a.BC[0] = 1
     return a
 
